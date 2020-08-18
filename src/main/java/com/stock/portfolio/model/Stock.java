aa5f361d17a2ef3196923583247ee1aed94e621d
Vinit Kamboj <vkamboj@hawk.iit.edu>
2020-08-18 11:40:39 -0500

    added new page

--- src/main/java/com/stock/portfolio/model/Stock.java
+++ src/main/java/com/stock/portfolio/model/Stock.java
@@ -16,6 +16,36 @@ public class Stock {
 	
 	@Column
 	private String StockName;
+	public int getStockId() {
+		return StockId;
+	}
+	public void setStockId(int stockId) {
+		StockId = stockId;
+	}
+	public String getStockName() {
+		return StockName;
+	}
+	public void setStockName(String stockName) {
+		StockName = stockName;
+	}
+	public double getStockValue() {
+		return StockValue;
+	}
+	public void setStockValue(double stockValue) {
+		StockValue = stockValue;
+	}
+	public String getStockSymbol() {
+		return StockSymbol;
+	}
+	public void setStockSymbol(String stockSymbol) {
+		StockSymbol = stockSymbol;
+	}
+	public double getStockPrice() {
+		return StockPrice;
+	}
+	public void setStockPrice(double stockPrice) {
+		StockPrice = stockPrice;
+	}
 	@Column
 	private double StockValue;
 	@Column
