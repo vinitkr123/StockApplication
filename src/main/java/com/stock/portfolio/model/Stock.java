bab7a0e1c024c7f4b80b39723e83e7c40667f046
gaurav.gupta <gauravbca15@gmail.com>
2020-08-30 12:21:16 +0530

    Change In Stock Model Unique value from true to false

--- src/main/java/com/stock/portfolio/model/Stock.java
+++ src/main/java/com/stock/portfolio/model/Stock.java
@@ -16,7 +16,7 @@ public class Stock {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int StockId;
 	
-	@Column(nullable = false,unique = true)
+	@Column(nullable = false,unique = false)
 	private String StockName;
 	public int getStockId() {
 		return StockId;
